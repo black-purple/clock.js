7ef30f105b609658f4c95510b09fd71b7d57ec37
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-04-09 22:31:22 +0000

    add: fully functional tabs

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+
+
+function clock() {
+    let time_values = document.querySelectorAll('.time_value');
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    h = (h<10) ? "0" + h : h;
+    m = (m<10) ? "0" + m : m;
+    s = (s<10) ? "0" + s : s;
+    
+    time_values[0].textContent = h;
+    time_values[1].textContent = m;
+    time_values[2].textContent = s;
+}
+
+var interval = setInterval(clock, 500);
