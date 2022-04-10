f4ee509643ec4ffe488f61159d4489b694b6e82b
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-04-10 01:31:05 +0000

    fix: removed js events from html

--- js/clock_toggle.js
+++ js/clock_toggle.js
@@ -1,6 +1,9 @@
 const analog_clock = document.querySelector("#analog_clock");
 const digital_clock = document.querySelector("#digital_clock");
 
+analog_clock.addEventListener('click', clockToggle);
+digital_clock.addEventListener('click', clockToggle);
+
 function clockToggle(){
     if (analog_clock.style.display == 'none'){
         analog_clock.style.display = 'block';
