974e54dd5f82fb3c1934f59c522c4f6f3c6fc2a5
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-04-11 20:42:19 +0000

    fix: disabling deadlines if 1 is active

--- js/timer_controller.js
+++ js/timer_controller.js
@@ -13,6 +13,8 @@ const hours_input = timer_setter.children[1];
 const minutes_input = timer_setter.children[4];
 const secs_input = timer_setter.children[7];
 
+const deadlines = document.querySelectorAll('.deadline');
+
 // Hours controller
 timer_setter.children[0].onclick = () => {
     let value = parseInt(hours_input.value);
@@ -109,8 +111,40 @@ timer_reset.onclick = () => {
     minutes_input.removeAttribute('disabled');
     secs_input.removeAttribute('disabled');
     
+    deadlines[0].removeAttribute('disabled');
+    deadlines[1].removeAttribute('disabled');
+    deadlines[2].removeAttribute('disabled');
+    deadlines[3].removeAttribute('disabled');
+    deadlines[4].removeAttribute('disabled');
+    deadlines[5].removeAttribute('disabled');
+    deadlines[6].removeAttribute('disabled');
+    deadlines[7].removeAttribute('disabled');
+    deadlines[8].removeAttribute('disabled');
     clearInterval(timeinterval);
 }
+
+function disableContent(){
+    timer_start.setAttribute('disabled', '');
+    hours_input.setAttribute('disabled', '');
+    minutes_input.setAttribute('disabled', '');
+    secs_input.setAttribute('disabled', '');
+    timer_setter.children[0].setAttribute('disabled', '');
+    timer_setter.children[2].setAttribute('disabled', '');
+    timer_setter.children[3].setAttribute('disabled', '');
+    timer_setter.children[5].setAttribute('disabled', '');
+    timer_setter.children[6].setAttribute('disabled', '');
+    timer_setter.children[8].setAttribute('disabled', '');
+    deadlines[0].setAttribute('disabled', '');
+    deadlines[1].setAttribute('disabled', '');
+    deadlines[2].setAttribute('disabled', '');
+    deadlines[3].setAttribute('disabled', '');
+    deadlines[4].setAttribute('disabled', '');
+    deadlines[5].setAttribute('disabled', '');
+    deadlines[6].setAttribute('disabled', '');
+    deadlines[7].setAttribute('disabled', '');
+    deadlines[8].setAttribute('disabled', '');
+}
+
 function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
@@ -139,16 +173,16 @@ let timeinterval = null;
 const audio = document.querySelector("audio");
 function initializeClock(id, endtime) {
         const clock = document.getElementById(id);
-        const hoursSpan = clock.querySelector('.hours');
-        const minutesSpan = clock.querySelector('.minutes');
-        const secondsSpan = clock.querySelector('.seconds');
+        const hours_div = clock.querySelector('.hours');
+        const minutes_div = clock.querySelector('.minutes');
+        const seconds_div = clock.querySelector('.seconds');
 
     function updateClock() {
         const t = getTimeRemaining(endtime);
         
-        hoursSpan.textContent = ('0' + t.hours).slice(-2);
-        minutesSpan.textContent = ('0' + t.minutes).slice(-2);
-        secondsSpan.textContent = ('0' + t.seconds).slice(-2);
+        hours_div.textContent = ('0' + t.hours).slice(-2);
+        minutes_div.textContent = ('0' + t.minutes).slice(-2);
+        seconds_div.textContent = ('0' + t.seconds).slice(-2);
 
         if (t.total <= 0) {
             audio.play();
@@ -171,59 +205,57 @@ function initializeClock(id, endtime) {
 }
 
 
-const deadlines = document.querySelectorAll('.deadline');
 deadlines[0].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 60 * 120 * 1 * 1000); // 2h
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[1].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 60 * 60 * 1 * 1000); // 1h
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[2].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 45 * 60 * 1000); // 45min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[3].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 30 * 60 * 1000); // 30min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[4].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 15 * 60 * 1000); // 15min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[5].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 10 * 60 * 1000); // 10min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[6].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 5 * 60 * 1000); // 5min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[7].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 1 * 60 * 1000); // 1min
     initializeClock('timer_text', deadline);
+    disableContent();
 }
 deadlines[8].onclick = () => {
     let deadline = new Date(Date.parse(new Date()) + 1 * 1 * 30 * 1000); // 30sec
     initializeClock('timer_text', deadline);
+    disableContent();
 }
-
 
 
 timer_start.onclick = () => {
     let deadline = new Date(
         Date.parse(new Date()) + (parseInt(hours_input.value) == 0 ? 1 : parseInt(hours_input.value) * 60) * (parseInt(minutes_input.value) == 0 ? 1 * 60 : parseInt(minutes_input.value) * 60) * (parseInt(secs_input.value) == 0 ? 1 : parseInt(secs_input.value)) * 1000);
     initializeClock('timer_text', deadline);
-    timer_start.setAttribute('disabled', '');
-    hours_input.setAttribute('disabled', '');
-    minutes_input.setAttribute('disabled', '');
-    secs_input.setAttribute('disabled', '');
-    timer_setter.children[0].setAttribute('disabled', '');
-    timer_setter.children[2].setAttribute('disabled', '');
-    timer_setter.children[3].setAttribute('disabled', '');
-    timer_setter.children[5].setAttribute('disabled', '');
-    timer_setter.children[6].setAttribute('disabled', '');
-    timer_setter.children[8].setAttribute('disabled', '');    
+    disableContent();
 }
 
