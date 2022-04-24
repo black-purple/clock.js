8e0c30bb5afe07d36a7febc5ca9b65f92fc7fc68
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-04-24 22:54:16 +0000

    feat: added jquery

--- js/clock_toggle.js
+++ js/clock_toggle.js
@@ -1,15 +1,8 @@
-const analog_clock = document.querySelector("#analog_clock");
-const digital_clock = document.querySelector("#digital_clock");
-
-analog_clock.onclick = () =>{
-    if (digital_clock.style.display == 'none' || !digital_clock.style.display){
-        digital_clock.style.display = 'block';
-        analog_clock.style.display = 'none';
-    }
-}
-digital_clock.onclick = () => {
-    if (analog_clock.style.display == 'none' || !analog_clock.style.display){
-        analog_clock.style.display = 'block';
-        digital_clock.style.display = 'none';
-    }
-}
+$('#analog_clock').click(() =>{
+    $('#analog_clock').hide();
+    $('#digital_clock').show();
+});
+$('#digital_clock').click(() =>{
+    $('#analog_clock').show();
+    $('#digital_clock').hide();
+});
