2aeb78bbebd5a83fcc237afcb3895b924ee2a596
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-04-24 22:54:58 +0000

    feat: added jquery

--- js/digital_clock.js
+++ js/digital_clock.js
@@ -1,18 +1,12 @@
 function clock() {
-    const time_values = document.querySelectorAll('.time_value');
-    
     let d = new Date();
     let h = d.getHours();
     let m = d.getMinutes();
     let s = d.getSeconds();
 
-    h = (h<10) ? "0" + h : h;
-    m = (m<10) ? "0" + m : m;
-    s = (s<10) ? "0" + s : s;
-    
-    time_values[0].textContent = h;
-    time_values[1].textContent = m;
-    time_values[2].textContent = s;
+    $('#timeValueHrs').text(h < 10 ? "0" + h : h);
+    $('#timeValueMins').text(m < 10 ? "0" + m : m);
+    $('#timeValueSecs').text(s < 10 ? "0" + s : s);
 }
 
 setInterval(clock, 1000);
